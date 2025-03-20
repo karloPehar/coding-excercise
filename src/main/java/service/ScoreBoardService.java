86b3e0add46ea8945448784116d3b02a27498137
karloPehar <karlopehar1@hotmail.com>
2025-03-20 16:04:52 +0100

    implemented validation for team names

--- src/main/java/service/ScoreBoardService.java
+++ src/main/java/service/ScoreBoardService.java
@@ -1,14 +1,23 @@
 package service;
 
 import model.Match;
-
+//import org.junit.platform.commons.util.StringUtils;
+//import org.junit.platform.commons.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 public class ScoreBoardService {
+
 
 
     public Match addNewMatch(String homeTeam, String awayTeam)
     {
+        if (StringUtils.isBlank(homeTeam) || StringUtils.isBlank(awayTeam))
+        {
+            throw new IllegalArgumentException("team names cannot be empty");
+        }
+
         Match newMatch = new Match(1,homeTeam,awayTeam);
 
         return newMatch;
     }
+
 }
